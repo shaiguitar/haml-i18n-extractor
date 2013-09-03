3eead0808832d9804c4496ea3003113f3322d07f
Shai Rosenfeld <srosenfeld@engineyard.com>
2013-09-03 13:36:32 -0700

    organize files in dirs

--- lib/haml-i18n-extractor.rb
+++ lib/haml-i18n-extractor.rb
@@ -1,22 +1,19 @@
-require "haml-i18n-extractor/version"
-require "haml-i18n-extractor/helpers"
-
-require "haml-i18n-extractor/text_finder"
-require "haml-i18n-extractor/exception_finder"
-
-
-require "haml-i18n-extractor/haml_parser"
-require "haml-i18n-extractor/haml_reader"
-
-require "haml-i18n-extractor/text_replacer"
-require "haml-i18n-extractor/haml_writer"
-require "haml-i18n-extractor/yaml_tool"
-
-require "haml-i18n-extractor/extractor"
-
-require "haml-i18n-extractor/prompter"
-require "haml-i18n-extractor/user_action"
-require "haml-i18n-extractor/tagging_tool"
-require "haml-i18n-extractor/workflow"
-require "haml-i18n-extractor/cli"
 require "trollop"
+
+require "haml-i18n-extractor/version"
+
+require "haml-i18n-extractor/extraction/text_finder"
+require "haml-i18n-extractor/extraction/exception_finder"
+require "haml-i18n-extractor/extraction/haml_parser"
+require "haml-i18n-extractor/extraction/haml_reader"
+require "haml-i18n-extractor/extraction/tagging_tool"
+require "haml-i18n-extractor/extraction/text_replacer"
+require "haml-i18n-extractor/extraction/haml_writer"
+require "haml-i18n-extractor/extraction/yaml_tool"
+require "haml-i18n-extractor/extraction/extractor"
+
+require "haml-i18n-extractor/flow/helpers"
+require "haml-i18n-extractor/flow/prompter"
+require "haml-i18n-extractor/flow/user_action"
+require "haml-i18n-extractor/flow/workflow"
+require "haml-i18n-extractor/flow/cli"
